54505ff00ad5f19bc3394f0dd818ea59284e6690
Dharm Vashisth <dharmvashisth5@gmail.com>
2026-04-12 21:25:57 +0530

    lazy dataframe practice using polars.

---
+++ scripts/data_frame_using_polars_lazy_evaluation.py
@@ -0,0 +1,27 @@
+import polars as pl
+import os
+
+def get_raw_data():
+    return {
+        "id":[1,2,3,4,5],
+        "name":["ab","ac","ad","ae","af"],
+        "salary":[14000,14000,15000,14000,16000],
+        "dept":["sales","market","sales","sales","market"]
+    }
+
+if __name__=="__main__":
+    file_path = os.path.join("..","data_files","employees.csv")
+    lazy_df = pl.scan_csv(file_path)
+
+    avg_sal_df = (
+        lazy_df.group_by(pl.col("dept")).agg(
+        pl.col("salary").mean().alias("Avg_salary"),
+        pl.col("salary").max().alias("Max_salary"),
+        pl.col("salary").min().alias("Min_salary"),
+        )
+    )
+    print(avg_sal_df.explain())
+
+    avg_sal_df.collect() # lazy dataframe will execute the operations
+    avg_sal_df.show()
+
